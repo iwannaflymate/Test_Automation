cfa0cb0ea1862b371b88e26663964fc8854780d2
aleksronzherenko <oleksandr.ronzherenko@devico.io>
2022-07-18 19:34:59 +0300

    Sign In page UI tests

---
+++ cypress/selectors/sign_in_page.js
@@ -0,0 +1,15 @@
+export const sign_in_page = {
+    empty_space: '.makeStyles-root-1',
+    app_logo: '.makeStyles-logo-3',
+    signIn_title: '.MuiTypography-h5',
+    signIn_button: '[data-test="signin-submit"]',
+    username_field: '#username',
+    username_label: '#username-label',
+    username_validation: '#username-helper-text',
+    password_field: '#password',
+    password_label: '#password-label',
+    password_validation: '#password-helper-text',
+    rememberMe_checkbox: '[type="checkbox"]',
+    signup_btn: '[data-test="signup"]',
+    cypress_lnk: '[target="_blank"]'
+}
