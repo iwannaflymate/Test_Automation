0c1e2b0c4bfe876dd1bf962a07f215b08c15ce62
aleksronzherenko <oleksandr.ronzherenko@devico.io>
2022-07-26 15:16:52 +0300

    Add custom commands and funcs helper

---
+++ cypress/helpers/funcs.js
@@ -0,0 +1,8 @@
+export const funcs = {
+    randomUsername() {
+        const random_username = ['Chris', 'Jake', 'Kenneth', 'Doyle', 'Jacob', 'Raymond', 'Daryl', 'Samantha', 'Denis', 'Webber', 'Donovan', 'Bradberry', 'Tenpenny', 'Johnson', 'Wexell', 'Teller', 'Wayne', 'Flame'];
+        const unIndex = Math.floor(Math.random() * random_username.length);
+        const UNrandomNumber = Math.floor(Math.random() * 10000);
+        return `${random_username[unIndex]}${UNrandomNumber}`;
+    }
+};
--- cypress/selectors/sign_up_page.js
+++ cypress/selectors/sign_up_page.js
@@ -6,10 +6,4 @@ export const sign_up_page = {
     fn_validation: '#firstName-helper-text',
     ln_validation: '#lastName-helper-text',
     cp_validation: '#confirmPassword-helper-text',
-    randomUsername() {
-        const random_username = ['Chris', 'Jake', 'Kenneth', 'Doyle', 'Jacob', 'Raymond', 'Daryl', 'Samantha', 'Denis', 'Webber', 'Donovan', 'Bradberry', 'Tenpenny', 'Johnson', 'Wexell', 'Teller', 'Wayne', 'Flame'];
-        const unIndex = Math.floor(Math.random() * random_username.length);
-        const UNrandomNumber = Math.floor(Math.random() * 10000);
-        return `${random_username[unIndex]}${UNrandomNumber}`;
-    }
 };
